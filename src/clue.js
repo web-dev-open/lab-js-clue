53f9c9a316d2df56f890e0b45eae77d188c15e27
ManishPoduval <manishpoduval@hotmail.com>
2022-09-02 09:49:34 +0530

    setup

---
+++ src/clue.js
@@ -0,0 +1,40 @@
+// ITERATION 1
+
+// Suspects Array
+
+const suspectsArray = [];
+
+// Rooms Array
+
+const roomsArray = [];
+
+// Weapons Array
+
+const weaponsArray = [];
+
+
+// ITERATION 2
+
+function selectRandom() {}
+
+function pickMystery() {}
+
+
+// ITERATION 3
+
+function revealMystery() {}
+
+
+
+// The following is required to make unit tests work.
+/* Environment setup. Do not modify the below code. */
+if (typeof module !== 'undefined') {
+  module.exports = {
+    suspectsArray,
+    roomsArray,
+    weaponsArray,
+    pickMystery,
+    revealMystery,
+    selectRandom
+  };
+}
